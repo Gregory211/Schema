d98a87d492672913045368a4f92acb0141e1a265
sundupeyv <v.sundupey@t1-group.ru>
2016-05-31 18:18:15 +0300

    first commit

---
+++ Sirena.Tests/SirenaClientTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+using NUnit.Framework;
+
+namespace Sirena.Tests
+{
+    [TestFixture]
+    public class SirenaClientTests
+    {
+        [Test]
+        public void ConnectTest()
+        {
+            var clientSettings = new SirenaClientSettings()
+            {
+                UserId = 922,
+                Host = "193.104.87.251",
+                Port = 34323,
+                PublicRsaKey = new RSAParameters()
+
+            };
+
+            SirenaClient client = new SirenaClient(clientSettings);
+
+            client.Connect();
+
+            Assert.IsTrue(client.IsConnected);
+        }
+    }
+}
---
+++ SirenaTravelProxyGateWcf/SirenaGateService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SirenaTravelProxyGateWcf
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "SirenaGateService" in both code and config file together.
+    public class SirenaGateService : ISirenaGateService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
