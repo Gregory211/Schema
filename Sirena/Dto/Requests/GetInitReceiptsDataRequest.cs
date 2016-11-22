335617213a2d06cae9354c6405607f4119091788
vanavaraVL <vanavaraVL@gmail.com>
2016-11-22 19:00:14 +0300

    get itin - for pdf only, wrong value type add get itin data - for download data for booking

---
+++ Sirena/Dto/Requests/GetInitReceiptsDataRequest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Sirena.Dto.Requests
+{
+    [DataContract, Serializable]
+    [XmlRoot("sirena")]
+    public class GetInitReceiptsDataRequest : DtoRequest
+    {
+        public GetInitReceiptsDataRequest() : base() { }
+
+        [DataMember]
+        [XmlElement("query")]
+        public GetInitReceiptsDataQuery Query { get; set; }
+    }
+
+    [DataContract, Serializable]
+    public sealed class GetInitReceiptsDataQuery
+    {
+        public GetInitReceiptsDataQuery() : base() { }
+
+        [DataMember]
+        [XmlElement("get_itin_receipts_data")]
+        public GetInitReceiptsDataParamas Params { get; set; }
+    }
+
+    [DataContract, Serializable]
+    public sealed class GetInitReceiptsDataParamas
+    {
+        public GetInitReceiptsDataParamas() : base() { }
+
+        [DataMember]
+        [XmlElement("regnum")]
+        public string Regnum { get; set; }
+
+        [DataMember]
+        [XmlElement("surname")]
+        public string Surname { get; set; }
+
+        [DataMember]
+        [XmlElement("standard_data")]
+        public bool StandartData { get; set; }
+    }
+}
